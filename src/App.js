84be84ddb33b46bbeee4c2f7d625523e702c85b6
Andrew Ammon <andrewammon@Andrews-MBP.home>
2018-07-10 10:58:51 -0400

    connected to chatkit api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import Chatkit from '@pusher/chatkit';
 import './App.css';
 import MessageList from './components/MessageList';
 import SendMessageForm from './components/SendMessageForm';
 import RoomList from './components/RoomList';
 import NewRoomForm from'./components/NewRoomForm';
 
+import { testToken, instanceLocator, roomId } from './index'
+
 class App extends Component {
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: instanceLocator,
+      userId: 'aammon',
+      tokenProvider: new Chatkit.TokenProvider({
+        url: testToken
+      })
+    })
+
+    chatManager.connect()
+    .then(currentUser => {
+      currentUser.subscribeToRoom({
+        roomId: roomId,
+        hooks: {
+          onNewMessage: message => {
+            console.log('message.text: ', message.text);
+          }
+        }
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const instanceLocator = 'v1:us1:1a417e33-fe32-4794-900c-532be077b49e';
+const testToken = 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/1a417e33-fe32-4794-900c-532be077b49e/token';
+const username = 'Andrew';
+const roomId = 11341760;
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+export { testToken, instanceLocator, username, roomId }
