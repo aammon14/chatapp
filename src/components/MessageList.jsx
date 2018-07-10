f4335f12066f95f1a27e5b928363ac324f6728fc
Andrew Ammon <andrewammon@Andrews-MBP.home>
2018-07-10 10:04:49 -0400

    render dummy messages

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 
+const DUMMY_DATA = [
+  {
+    senderId: 'Andrew',
+    text: 'Hey, how is it going?'
+  },
+  {
+    senderId: 'John',
+    text: 'It\'s going well, how about you?'
+  },
+  {
+    senderId: 'Andrew',
+    text: 'That\'s great! I\'m also doing well!'
+  }
+]
+
 class MessageList extends Component {
   render() {
     return (
       <div className="message-list">
-        <div className="help=text">MessageList</div>
+        {DUMMY_DATA.map((message, index) => {
+          return (
+            <div key={index} className="message">
+              <div className="message-username">{message.senderId}</div>
+              <div className="message-text">{message.text}</div>
+            </div>
+          )
+        })}
       </div>
     )
   }
